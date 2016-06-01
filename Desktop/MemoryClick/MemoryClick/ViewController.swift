7d9d6ef3fe3f99de7aa4ca9937e59ab6a1c80e27
yangguozhanzhao <728074993@qq.com>
2016-06-01 20:20:00 +0800

    initial commit

---
+++ Desktop/MemoryClick/MemoryClick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryClick
+//
+//  Created by 杨展 on 14/9/13.
+//  Copyright (c) 2014年 8210. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Desktop/MemoryClick/MemoryClickTests/MemoryClickTests.swift
@@ -0,0 +1,36 @@
+//
+//  MemoryClickTests.swift
+//  MemoryClickTests
+//
+//  Created by 杨展 on 14/9/13.
+//  Copyright (c) 2014年 8210. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MemoryClickTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
